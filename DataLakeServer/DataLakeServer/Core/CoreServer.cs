4329f5ab2459cee4ab9c99bd83ca05c9b01be323
qwinner <xiaoqiangaaa2007@163.com>
2015-12-28 17:30:57 +0800

    delete server sharp . plan to use IOCP.

--- DataLakeServer/DataLakeServer/Core/CoreServer.cs
+++ DataLakeServer/DataLakeServer/Core/CoreServer.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebSocketSharp.Server;
+
 
 namespace DataLakeServer.Core
 {
     public class CoreServer
     {
         private CoreServer instance = new CoreServer();
-        private WebSocketServer mWssv;
+
         public CoreServer GetInstance()
         {
             return instance;
@@ -23,16 +23,12 @@ namespace DataLakeServer.Core
         public void InitInstance()
         {    
             
-            mWssv = new WebSocketServer(4649);
-            mWssv.AddWebSocketService<EchoService>("/Echo");
-            mWssv.AddWebSocketService<ChatService>("/Chat");
-            mWssv.AddWebSocketService<ChatService>("/ChatWithNyan", () => new ChatService(" Nyan!"));
-            mWssv.Start();            
+           
         }
 
         ~CoreServer()
         {
-            mWssv.Stop();
+
         }
     }
 }
